9e54dd3f8442993dfd4cec0ddd3e07deee900ffc
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-11-16 19:32:59 +0900

    [feat/#3] app,repoistory DI

---
+++ app/src/main/java/com/bendeng/stockmarketpraticeapp/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.bendeng.stockmarketpraticeapp.di
+
+import com.bendeng.stockmarketpraticeapp.data.csv.CSVParser
+import com.bendeng.stockmarketpraticeapp.data.csv.CompanyListingsParser
+import com.bendeng.stockmarketpraticeapp.data.repository.StockRepositoryImpl
+import com.bendeng.stockmarketpraticeapp.domain.model.CompanyListing
+import com.bendeng.stockmarketpraticeapp.domain.repository.StockRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingsParser: CompanyListingsParser,
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl,
+    ): StockRepository
+}
