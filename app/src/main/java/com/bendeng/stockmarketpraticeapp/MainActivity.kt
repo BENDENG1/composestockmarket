0da251697e9539f2926d64d5df6f24f40463c158
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-11-16 19:35:01 +0900

    [feat/#3] CompanyItem 구현

--- app/src/main/java/com/bendeng/stockmarketpraticeapp/MainActivity.kt
+++ app/src/main/java/com/bendeng/stockmarketpraticeapp/MainActivity.kt
@@ -6,42 +6,33 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.bendeng.stockmarketpraticeapp.presentation.company_listings.NavGraph
+import com.bendeng.stockmarketpraticeapp.presentation.company_listings.NavGraphs
 import com.bendeng.stockmarketpraticeapp.ui.theme.StockMarketPraticeAppTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             StockMarketPraticeAppTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StockMarketPraticeAppTheme {
-        Greeting("Android")
     }
 }
