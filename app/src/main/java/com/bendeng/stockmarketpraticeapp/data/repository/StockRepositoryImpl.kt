fbf7c298345d62e2ec7cdc03a6c7076699a36611
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-11-17 15:38:45 +0900

    [feat/#5] 회사 정보repository, parser 구현

--- app/src/main/java/com/bendeng/stockmarketpraticeapp/data/repository/StockRepositoryImpl.kt
+++ app/src/main/java/com/bendeng/stockmarketpraticeapp/data/repository/StockRepositoryImpl.kt
@@ -2,12 +2,14 @@ package com.bendeng.stockmarketpraticeapp.data.repository
 
 import coil.network.HttpException
 import com.bendeng.stockmarketpraticeapp.data.csv.CSVParser
-import com.bendeng.stockmarketpraticeapp.data.csv.CompanyListingsParser
 import com.bendeng.stockmarketpraticeapp.data.loacal.StockDatabase
+import com.bendeng.stockmarketpraticeapp.data.mapper.toCompanyInfo
 import com.bendeng.stockmarketpraticeapp.data.mapper.toCompanyListing
 import com.bendeng.stockmarketpraticeapp.data.mapper.toCompanyListingEntity
 import com.bendeng.stockmarketpraticeapp.data.remote.StockApi
+import com.bendeng.stockmarketpraticeapp.domain.model.CompanyInfo
 import com.bendeng.stockmarketpraticeapp.domain.model.CompanyListing
+import com.bendeng.stockmarketpraticeapp.domain.model.IntraDayInfo
 import com.bendeng.stockmarketpraticeapp.domain.repository.StockRepository
 import com.bendeng.stockmarketpraticeapp.util.Resource
 import kotlinx.coroutines.flow.Flow
@@ -21,6 +23,7 @@ class StockRepositoryImpl @Inject constructor(
     private val api: StockApi,
     private val db: StockDatabase,
     private val companyListingsParser: CSVParser<CompanyListing>,
+    private val intraDayInfoParser: CSVParser<IntraDayInfo>,
 ) : StockRepository {
 
     private val dao = db.dao
@@ -65,6 +68,34 @@ class StockRepositoryImpl @Inject constructor(
                 ))
                 emit(Resource.Loading(false))
             }
+        }
+    }
+
+    override suspend fun getIntraDayInfo(symbol: String): Resource<List<IntraDayInfo>> {
+        return try {
+            val response = api.getIntraDayInfo(symbol)
+            val results = intraDayInfoParser.parse(response.byteStream())
+
+            Resource.Success(results)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Resource.Error(message = "주식의 차트를 불러올 수 없습니다.")
+        } catch (e: HttpException) {
+            e.printStackTrace()
+            Resource.Error(message = "주식의 차트를 불러올 수 없습니다.")
+        }
+    }
+
+    override suspend fun getCompanyInfo(symbol: String): Resource<CompanyInfo> {
+        return try {
+            val result = api.getCompanyInfo(symbol = symbol)
+            Resource.Success(result.toCompanyInfo())
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Resource.Error(message = "회사 정보를 불러올 수 없습니다.")
+        } catch (e: HttpException) {
+            e.printStackTrace()
+            Resource.Error(message = "회사 정보를  불러올 수 없습니다.")
         }
     }
 }
--- app/src/main/java/com/bendeng/stockmarketpraticeapp/domain/repository/StockRepository.kt
+++ app/src/main/java/com/bendeng/stockmarketpraticeapp/domain/repository/StockRepository.kt
@@ -1,6 +1,8 @@
 package com.bendeng.stockmarketpraticeapp.domain.repository
 
+import com.bendeng.stockmarketpraticeapp.domain.model.CompanyInfo
 import com.bendeng.stockmarketpraticeapp.domain.model.CompanyListing
+import com.bendeng.stockmarketpraticeapp.domain.model.IntraDayInfo
 import com.bendeng.stockmarketpraticeapp.util.Resource
 import kotlinx.coroutines.flow.Flow
 
@@ -10,4 +12,12 @@ interface StockRepository {
         fetchFromRemote: Boolean,
         query: String,
     ): Flow<Resource<List<CompanyListing>>>
+
+    suspend fun getIntraDayInfo(
+        symbol: String,
+    ): Resource<List<IntraDayInfo>>
+
+    suspend fun getCompanyInfo(
+        symbol: String,
+    ): Resource<CompanyInfo>
 }
